899874bbe1a7c4a975ce2cb1f2eab22fdf4999cd
Frank A Pol <fpol@usc.edu>
2020-07-24 13:02:51 -0700

    finshed

--- pymel/BrowAndLidControlScript.py
+++ pymel/BrowAndLidControlScript.py
@@ -8,60 +8,45 @@ set_driven_key_flag = True
 sides = ['l', 'r']
 verts = ['top', 'bot']
 #if this gets too bloated, read from json file
-control_transform_dict = {'eyebrow':{'inner':{'inner':{'ty':1.5}}, 'mid':{'mid':{'ty':1.5}}, 'outer':{'outer':{'ty':1.5}}, 'notch':{'notch':{'ty':1.5}}}}
-#lid_names = ['mid', 'front', 'outer', 'inner']
-#lid_names_to_modifiers = {'mid':{'tx':1.5, 'sz':.2, 'sx':.2}, 'front':{'ty':1.5}, 'outer':{'ty':1.5}, 'inner':{'ty':1.5}}
-#brow_names = ['inner', 'mid', 'outer', 'notch']
-#brow_long_names = ['inner', 'mid', 'outer', 'inner_selector|notch']
+control_transform_dict = {'eyebrow':{'inner':{'inner':{'ty':1.5, 'rz':90}}, 'mid':{'mid':{'ty':1.5, 'rz':90}}, 'outer':{'outer':{'ty':1.5, 'rz':90}}, 'notch':{'notch':{'ty':1.5, 'rz':1.5}}}}
+control_transform_dict['eyelid'] = {'mid':{'mid':{'ty':1.5}}, 'inner':{'inner':{'ty':1.5}}, 'front':{'front':{'ty':1.5}}, 'outer':{'outer':{'ty':1.5}}}
 
-driverBaseValue = 1.5
-drivenMultiplier = 1.5
+driverBaseValue = 1
 
 for side in sides:
-    side_flip_flag = 1
+    flip_flag = 1
     if side == 'r':
-        side_flip_flag = -1
-    #eyebrow control connections
-    for b in range(0,len(brow_names)):
-        curr_joint = PyNode('j_'+side+'_eyebrow_'+brow_names[b])
-        curr_control = PyNode(side+'_eyebrow_controls|'+brow_long_names[b]+'_selector')
-        if cut_key_flag:
-            cutKey(curr_joint.ty)
-        if set_driven_key_flag:
-            drivenValueLow = driverBaseValue * drivenMultiplier * -1 + curr_joint.ty.get()
-            drivenValueHigh = driverBaseValue * drivenMultiplier  + curr_joint.ty.get()
-            setDrivenKeyframe(curr_joint.ty, cd=curr_control.ty, driverValue=driverBaseValue*-1, v=drivenValueLow)
-            setDrivenKeyframe(curr_joint.ty, cd=curr_control.ty, driverValue=driverBaseValue, v=drivenValueHigh)
-            
-    #eyebrow control connections
-    for vert in verts:
-        for curr_spec in lid_names_to_modifiers:
-            if curr_spec ==  'mid' and vert == '_top_':
-                print(curr_spec, side_flip_flag)
-                side_flip_flag *= -1
-            elif side == 'r':
-                side_flip_flag = -1
-            print(curr_spec, side_flip_flag)
-            print(lid_names_to_modifiers[curr_spec])
-            curr_joint = PyNode('j_'+side+'_'+vert+'_eyelid_'+curr_spec)
-            curr_control = PyNode(side+'_'+vert+'_eyelid_controls|'+curr_spec+'_selector')
-            for curr_attr in lid_names_to_modifiers[curr_spec]:
-                print(side, side_flip_flag)
-                curr_joint_attr = eval('curr_joint.'+curr_attr)
-                curr_multiplier = lid_names_to_modifiers[curr_spec][curr_attr] 
-                if cut_key_flag:
-                    cutKey(curr_joint_attr)
-                if set_driven_key_flag:
-                        drivenValueLow = driverBaseValue * curr_multiplier * -1 * side_flip_flag + curr_joint_attr.get()
-                        drivenValueHigh = driverBaseValue * curr_multiplier * side_flip_flag + curr_joint_attr.get()
-                        setDrivenKeyframe(curr_joint_attr, cd=curr_control.ty, driverValue=-1*driverBaseValue, v=drivenValueLow)
-                        setDrivenKeyframe(curr_joint_attr, cd=curr_control.ty, driverValue=driverBaseValue, v=drivenValueHigh)
-            #if lid_names[c] == 'front':
-            #    #trying to stick eyelids to eye without geo constraint
-            #    zDrivenMultiplier = .2
-            #    curr_joint_attr = curr_joint.tz
-            #    drivenValueLow = -1 * driverBaseValue * zDrivenMultiplier + curr_joint_attr.get()
-            #    drivenValueMid = curr_joint_attr.get()
-            #    setDrivenKeyframe(curr_joint_attr, cd=curr_control.ty, driverValue=-1*driverBaseValue, v=drivenValueLow)
-            #    setDrivenKeyframe(curr_joint_attr, cd=curr_control.ty, driverValue=0, v=drivenValueMid)
-            #    setDrivenKeyframe(curr_joint_attr, cd=curr_control.ty, driverValue=driverBaseValue, v=drivenValueLow)
+        flip_flag = -1
+    for curr_group_name in control_transform_dict:
+        verts_mod = ['']
+        if curr_group_name == 'eyelid':
+            verts_mod = ['top_', 'bot_']
+        else:
+            flip_flag = 1
+        for vert in verts_mod:
+            for curr_control_name in control_transform_dict[curr_group_name]:
+                curr_control = PyNode(side+'_'+vert+curr_group_name+'_controls|'+curr_control_name+'_selector')
+                print(curr_control)
+                for curr_joint_name in control_transform_dict[curr_group_name][curr_control_name]:
+                    curr_joint = PyNode('j_'+side+'_'+vert+curr_group_name+'_'+curr_joint_name)
+                    print(curr_joint)
+                    for curr_attr_name in control_transform_dict[curr_group_name][curr_control_name][curr_joint_name]:
+                        if 'r' in curr_attr_name:
+                            driverBaseValue = 90
+                        else:
+                            driverBaseValue = 1
+                        cutKey(eval('curr_joint.'+curr_attr_name))
+                        curr_mult = control_transform_dict[curr_group_name][curr_control_name][curr_joint_name][curr_attr_name]
+                        drivenValueLow = curr_mult * -1 * flip_flag + eval('curr_joint.'+curr_attr_name).get()
+                        drivenValueHigh = curr_mult * flip_flag + eval('curr_joint.'+curr_attr_name).get()
+                        setDrivenKeyframe(eval('curr_joint.'+curr_attr_name), cd=eval('curr_control.'+curr_attr_name), driverValue=driverBaseValue*-1, v=drivenValueLow)
+                        setDrivenKeyframe(eval('curr_joint.'+curr_attr_name), cd=eval('curr_control.'+curr_attr_name), driverValue=driverBaseValue, v=drivenValueHigh)
+                    #     curr_joint = PyNode('j_'+side+'_'+curr_group_name+'_'+curr_joint_name)
+                    #     curr_control = PyNode(side+'_'+curr_group_name+'_controls|'+brow_long_names[b]+'_selector')
+                    #     if cut_key_flag:
+                    #         cutKey(curr_joint.ty)
+                    #     if set_driven_key_flag:
+                    #         drivenValueLow = driverBaseValue * drivenMultiplier * -1 + curr_joint.ty.get()
+                    #         drivenValueHigh = driverBaseValue * drivenMultiplier  + curr_joint.ty.get()
+                    #         setDrivenKeyframe(curr_joint.ty, cd=curr_control.ty, driverValue=driverBaseValue*-1, v=drivenValueLow)
+                    #         setDrivenKeyframe(curr_joint.ty, cd=curr_control.ty, driverValue=driverBaseValue, v=drivenValueHigh)
