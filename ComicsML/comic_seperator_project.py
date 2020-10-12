b63845423fc9a12970119496ee3bcda1788165a4
Frank A Pol <fpol@usc.edu>
2020-10-12 15:30:53 -0700

    filling pages and elementary though bubble placement

--- ComicsML/comic_seperator_project.py
+++ ComicsML/comic_seperator_project.py
@@ -2,14 +2,14 @@ from PIL import Image
 import random
 import json
 
-img = Image.open('./building_blocks/speech-bubbles.png', 'r')
-img_w = img.size[0]//2
-img_h = img.size[1]//2
+sq_img = Image.open('./building_blocks/sq.png', 'r')
+sp_img = Image.open('./building_blocks/speech-bubble.png', 'r')
 page_sizes = [(920, 1375), (800, 1145)]
 page_bg_colors = [(255, 255, 255, 255), (122, 122, 122, 255), (0, 0, 0, 255)]
 build_order = -1 #manga style or western style orders
 min_frame_dim = 15
 max_frame_dim = 50
+min_speech_dim = min_frame_dim/5
 
 for i in range (1,10):
     #Page feature generation
@@ -26,26 +26,67 @@ for i in range (1,10):
     metadata['page_padding'] = page_padding
 
     panel_idx = 1
-    x_offset = page_padding
     y_offset = page_padding
-    while x_offset < 100 - page_padding:
-        panel_width = random.randint(min_frame_dim,max_frame_dim)
-        x_px_offset = int(bg_w*(x_offset/100))
-        panel = img.resize((int(panel_width/100*bg_w), int(panel_width/100*bg_w)))
-        panel_offset = (x_px_offset, x_px_offset)
+    while y_offset < 100 - page_padding:
+        x_offset = page_padding
+        panel_height = random.randint(min_frame_dim,max_frame_dim)
+        y_px_offset = int(bg_h*(y_offset/100))
         panel_margin = random.randint(0,10)
+        new_y_offset = y_offset + panel_height + panel_margin
 
-        #Page metadata
-        panel_metadata = {}
-        panel_metadata['id'] = panel_idx
-        panel_metadata['size'] = panel_width
-        panel_metadata['position'] = x_offset
-        metadata['panel_id_'+str(panel_idx)] = panel_metadata
-        panel_idx = panel_idx + 1
+        while x_offset < 100 - page_padding:
+            panel_width = random.randint(min_frame_dim,max_frame_dim)
+            x_px_offset = int(bg_w*(x_offset/100))
+            panel = sq_img.resize((int(panel_width/100*bg_w), int(panel_height/100*bg_h)))
+            panel_offset = (x_px_offset, y_px_offset)
 
-        background.paste(panel, panel_offset)
-        x_offset = min(x_offset + panel_width + panel_margin, bg_w - page_padding)
+            #Panel metadata
+            panel_metadata = {}
+            panel_metadata['id'] = panel_idx
+            panel_metadata['height'] = panel_height
+            panel_metadata['width'] = panel_width
+            panel_metadata['y_position'] = y_offset
+            panel_metadata['x_position'] = x_offset
+            panel_idx = panel_idx + 1
+
+            background.paste(panel, panel_offset)
+
+            new_x_offset = x_offset + panel_width + panel_margin
+
+            #speech bubble insertion
+            speech_idx = 1;
+            speech_y_offset = y_offset
+            while speech_y_offset < new_y_offset - min_speech_dim:
+                speech_x_offset = x_offset
+                speech_height = random.randint(min_speech_dim, new_y_offset-speech_y_offset)
+                speech_y_px_offset = int(bg_h*(speech_y_offset/100))
+                while speech_x_offset < new_x_offset - min_speech_dim:
+                    speech_width = random.randint(min_speech_dim,new_x_offset-speech_x_offset)
+                    speech_x_px_offset = int(bg_w*(speech_x_offset/100))
+                    speech = sp_img.resize((int(speech_width/100*bg_w), int(speech_height/100*bg_h)))
+                    speech_offset = (speech_x_px_offset, speech_y_px_offset)
+                    
+                    #bubble metadata
+                    speech_metadata = {}
+                    speech_metadata['id'] = speech_idx
+                    speech_metadata['height'] = speech_height
+                    speech_metadata['width'] = speech_width
+                    speech_metadata['y_position'] = speech_y_offset
+                    speech_metadata['x_position'] = speech_x_offset
+                    speech_idx = speech_idx + 1
+                    background.paste(speech, speech_offset)
+                    panel_metadata['speech_id_'+str(speech_idx)] = speech_metadata
+
+                    speech_x_offset = speech_x_offset + speech_width + random.randint(0,new_x_offset-speech_x_offset)
+
+                speech_y_offset = speech_y_offset + speech_height + random.randint(0,new_y_offset-speech_y_offset)
+            #save metadata
+            metadata['panel_id_'+str(panel_idx)] = panel_metadata
+            x_offset = min(new_x_offset, 100 - page_padding)
+
+        print(new_y_offset)
+        y_offset = min(new_y_offset, 100 - page_padding)
 
     background.save('./training/training_'+str(i)+'.png')
     with open('./training/training_'+str(i)+'.json', 'w') as outfile:
-        json.dump(metadata, outfile)
+        json.dump(metadata, outfile, indent=4, sort_keys=True)
