04a6b0a047a9a97d72bcff82bda1acebeeaeeb23
Frank A Pol <fpol@usc.edu>
2021-04-04 14:36:44 -0700

    HS Image download script

--- imgSetDL.py
+++ imgSetDL.py
@@ -6,21 +6,29 @@
 from PIL import Image
 import requests
 
-im = Image.open(requests.get(url, stream=True).raw)
+url = 'http://worldagnetwork.com/'
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+result = requests.get(url, headers=headers)
+print(result.content.decode())
 
 for cid in range(999):
 	cid_str = str(cid)
 	cid_str = cid_str.zfill(3)
-	# print(cid_str)
+	print(cid_str)
 	try:
-		req = Request("https://art.hearthstonejson.com/v1/orig/BAR_"+cid_str+".png", headers={'User-Agent': 'Mozilla/5.0'})
-		# webpage = urlopen(req).read()
-		with urlopen(req) as response:
-			print(response)
-			# f = open("Cards/Barrens/BAR_"+cid_str+".png","w+")
-			im = Image.open(response.raw)
-			im.save("Cards/Barrens/BAR_"+cid_str+".png")
-			# f.close()
+		res = requests.get("https://art.hearthstonejson.com/v1/orig/BAR_"+cid_str+".png", headers={'User-Agent': 'Mozilla/5.0'})
+		file = open("sample_image.png", "wb")
+		file = open("Cards/Barrens/BAR_"+cid_str+".png", "wb")
+		file.write(res.content)
+		file.close()
+
+		# print(res.content.decode())
+		# print("1")
+		# im = Image.open(res.raw.data)
+		# print("2")
+		# im.save("Cards/Barrens/BAR_"+cid_str+".png")
+		# print("BAR_"+cid_str+".png saved")
+		# f.close()
 		# req.urlretrieve("https://art.hearthstonejson.com/v1/orig/BAR_"+cid_str+".png", "/Cards/Barrens/cid_str.png")
 	except Exception as e: 
 		# print(cid)
